e6588ff8faf09edeaa409ad40a212073f12521fe
Superschnizel <llvelt@yahoo.de>
2024-07-20 13:21:13 +0200

    node rebuilding first working version

--- src/rendering/TextColorPostProcessor.ts
+++ src/rendering/TextColorPostProcessor.ts
@@ -15,44 +15,23 @@ export const textColorPostProcessor = (el: HTMLElement, context: MarkdownPostPro
 	let themeName = context.frontmatter ? context.frontmatter["ftcTheme"] : null;
 	themeName = themeName ? themeName : getCurrentTheme(settings).name;
 
-	// This is a hacky way of doing this, but every other possibility seemed incredibly convoluted.
-	// For now to handle the codeblocks will require these weird splits.
-	//
-	// I tried doing this another way (see version 1.0.5) but that caused some very weird issues with themes and css.
 	console.log(prettyPrintDOMStructure(el));
 
-	rebuildNode(el, themeName);
-	return;
+	const emergencyCopy = el.cloneNode(true);
 
-	const split = el.innerHTML.split(/\<code/g);
+	try {
+		rebuildNode(el, themeName);
+	} catch (e) {
+		console.error(`fatal in rebuildNode: ${e}`)
+		// readd from emergency Copy. should be removed as soon as node rebuilding is stable.
+		el.childNodes.forEach(c => {
+			c.parentNode?.removeChild(c);
+		})
 
-	let inner = '';
-
-	for (let i = 0; i < split.length; i++) {
-		if (i % 2 == 0) {
-			inner += split[i].replace(PREFIX, (match) => {
-				return `<span class="${CSS_COLOR_PREFIX}${themeName}-${match.slice(3, match.length - 1)}">`;
-			}).replace(SUFFIX, "</span>")
-			continue;
-		}
-
-		inner += "<code"
-		const innerSplit = split[i].split("</code>")
-
-		for (let j = 0; j < innerSplit.length; j++) {
-			if (j % 2 == 0) {
-				inner += innerSplit[j];
-				continue;
-			}
-			inner += "</code>" + innerSplit[i].replace(PREFIX, (match) => {
-				return `<span class="${CSS_COLOR_PREFIX}${themeName}-${match.slice(3, match.length - 1)}">`;
-			}).replace(SUFFIX, "</span>")
-
-		}
+		emergencyCopy.childNodes.forEach(c => {
+			el.appendChild(c);
+		})
 	}
-
-	el.innerHTML = inner;
-
 	return;
 }
 
@@ -74,53 +53,38 @@ function rebuildNode(node: Node, themeName: string, level: number = 0, nodeStack
 
 	}
 
-	// if (!node.textContent?.match(PREFIX) && !node.textContent?.match(SUFFIX)) {
-	// 	return node;
-	// }
+	// keep track of last length of live nodeList to handle items moved out of list.
+	let lastLength = node.childNodes.length;
 
-	// if (!node.hasChildNodes()) {
-	// 	console.log("no childs?");
-	// 	
-	// 	return node;
-	// }
+	for (let i = 0; i < node.childNodes.length; i++) {
 
-	// create a stack to manage state
-	// const nodeStack: Node[] = [];
+		lastLength = node.childNodes.length;
 
-	// get the children 
-	// const newNode = node.cloneNode(false);
-	// const newNode = document.createDocumentFragment().createDiv();
 
-	// remove all copied children
-	// while (node.firstChild) {
-	// 	node.removeChild(node.firstChild)
-	// }
+		let childNode: Node = node.childNodes.item(i);
 
-	// let children : Node[] = [];
-	// node.childNodes.forEach(n => {
-	// 	children.push(n.cloneNode(true));
-	// })
+		// node.childNodes.forEach((childNode: Node) => {
 
-	nodeStack.push(node);
-
-	let children = node.childNodes;
-	node.childNodes.forEach((childNode: Node) => {
-
-		if (childNode == nodeStack.last()) {
-			// can happen because of appending
-			return;
-		}
+		// if (childNode == nodeStack.last()) {
+		// 	// can happen because of appending
+		// 	return;
+		// }
 
 		const text = childNode.nodeValue;
-		console.log(`node: ${childNode.nodeName} ==> ${text}`);
 		// console.log(childNode.compareDocumentPosition(nodeStack.last()!));
 
 		// the last item on the stack should always be the current parent.
-		if (!(childNode.compareDocumentPosition(nodeStack.last()!) & Node.DOCUMENT_POSITION_CONTAINS)) {
-			console.log("setting new parent");
+		if ((nodeStack.last() != undefined) && (nodeStack.last() != childNode) && !(childNode.compareDocumentPosition(nodeStack.last()!) & Node.DOCUMENT_POSITION_CONTAINS)) {
+			// if (!(childNode.compareDocumentPosition(nodeStack.last()!) & Node.DOCUMENT_POSITION_CONTAINS) && nodeStack.last() != childNode) {
+			console.log(`setting new parent for ${childNode.nodeName}`);
 
 			childNode.parentNode?.removeChild(childNode);
 			nodeStack.last()?.appendChild(childNode);
+
+			// deal with elements moved outside of list.
+			if (lastLength > node.childNodes.length) {
+				i -= lastLength - node.childNodes.length;
+			}
 		}
 
 		// console.log(`node: ${childNode.nodeName}, level: ${level}`);
@@ -130,157 +94,80 @@ function rebuildNode(node: Node, themeName: string, level: number = 0, nodeStack
 			// console.log(`rbuilding and appending ${childNode.nodeName} to ${nodeStack.last()?.nodeName}`);
 
 			// nodeStack.last()?.appendChild(rebuildNode(childNode, themeName, level + 1));
-			return;
-		}
-
-		if (text == null || text == "") {
-			return;
-		}
-
-		// node has one or more opening delimiters in it.
-
-		// get the list of prefixes and suffixes in the text node.
-		let prefixes = GetAllMatches(text, PREFIX).reverse();
-		let suffixes = GetAllMatches(text, SUFFIX).reverse();
-		let lastpos = 0;
-
-		// console.log(`prefixes: ${prefixes.reduce((prev, next) => prev + `, ${next.index}`, "")}`);
-		// console.log(`suffixes: ${suffixes.reduce((prev, next) => prev + `, ${next.index}`, "")}`);
-
-		if ((prefixes.length <= 0) && (suffixes.length <= 0)) {
-			// exit out if no matches found.
-			return;
-		}
-
-		let count = 0;
-
-		let lastInsertedNode = childNode;
-
-		while ((prefixes.length > 0) || (suffixes.length > 0)) {
-			let nextPrefixPosition = prefixes.last()?.index ?? Number.POSITIVE_INFINITY;
-			let nextSuffixPosition = suffixes.last()?.index ?? Number.POSITIVE_INFINITY;
-
-			// if (nodeStack.last()! != childNode.parentNode) {
-			// 	console.log("setting new parent");
-			//
-			// 	childNode.parentNode?.removeChild(childNode);
-			// 	nodeStack.last()?.appendChild(childNode);
-			// }
-
-			count++;
-			if (count > 1000) {
-				console.log(`count was too large, next: pre=${nextPrefixPosition}, suf=${nextSuffixPosition}`);
-			}
-
-			if (nextPrefixPosition == nextSuffixPosition) {
-				// should never be the case but idk.
-				console.log("nextPre and nextSuf are the same!!: " + `${nextPrefixPosition}`);
-
-				return;
-			}
-
-
-			if (nextPrefixPosition < nextSuffixPosition) {
-				// next is prefix
-				let textBeforeDelim = text.slice(lastpos, nextPrefixPosition);
-				let textAfterDelim = text.slice(prefixes.last()!.end);
-				let prefix = prefixes.last()!.value;
-				let color = prefix.slice(3, prefix.length - 1);
-
-				console.log(`handling prefix:\nprevText: ${textBeforeDelim}`)
-
-				// nodeStack.last()?.insertAfter(document.createTextNode(prevText), referenceNode);
-
-				// var newPrevNode = document.createTextNode(prevText);
-				// var reference = childNode.parentNode == nodeStack.last()! ? childNode : nodeStack.last()!.lastChild;
-				// nodeStack.last()?.insertAfter(newPrevNode, reference);
-
-				// nodeStack.last()?.replaceChild(document.createTextNode(prevText), childNode);
-
-				// if (nodeStack.last()! != childNode.parentNode) {
-				// 	childNode.parentNode?.removeChild(childNode);
-				// 	nodeStack.last()?.appendChild(childNode);
-				// }
-
-				let colorSpan = document.createElement("span");
-				colorSpan.addClass(`${CSS_COLOR_PREFIX}${themeName}-${color}`);
-
-				// set text in last node and create color node and insert it after the last node. 
-				lastInsertedNode.nodeValue = textBeforeDelim;
-				nodeStack.last()?.insertAfter(colorSpan, lastInsertedNode);
-
-				// add an empty text node to the span and set is as te lastInsertedNode
-				// this will be filled in the next iteration.
-				let newNode = document.createTextNode("");
-				colorSpan.appendChild(newNode);
-
-				lastInsertedNode = newNode;
-
-				// set the colorSpan to be the new parent.
-				nodeStack.push(colorSpan);
-
-				lastpos = prefixes.last()!.end;
-
-				// childNode.nodeValue = textBeforeDelim;
-
-				// colorSpan.appendChild(document.createTextNode(textAfterDelim));
-
-				// remove prefix from match.
-				prefixes.pop();
-
-				continue;
-			}
-			// next is suffix;
-
-			let textBeforeDelim = text.slice(lastpos, nextSuffixPosition);
-			// let textAfterDelim = text.slice(suffixes.last()!.end);
-
-			lastInsertedNode.nodeValue = textBeforeDelim;
-
-			let prevNode = nodeStack.pop()!;
-
-			let newNode = document.createTextNode("");
-			nodeStack.last()?.insertAfter(newNode, prevNode);
-			lastInsertedNode = newNode;
-			// nodeStack.last()?.insertAfter(document.createTextNode(textAfterDelim), prevNode);
-			// let prevText = text.slice(lastpos, nextSuffixPosition);
-			// // childNode.parentNode?.removeChild(childNode);
-			// nodeStack.last()?.appendChild(document.createTextNode(prevText));
-
-			// console.log(`handling suffix:`)
-
-			lastpos = suffixes.last()!.end;
-			//
-
-			suffixes.pop();
-
 			continue;
 		}
 
-		// remove original child node
-		// childNode.parentNode?.removeChild(childNode);
+		console.log(`node: ${childNode.nodeName}\n  text: ${text}\n  index: ${i}\n  childNode.length: ${node.childNodes.length}\n  lastLength: ${lastLength}`);
 
-		if (lastpos <= text.length) {
-			let leftOverText = text.slice(lastpos);
-			lastInsertedNode.nodeValue = leftOverText;
+		if (text == null || text == "") {
+			continue;
 		}
-	});
 
-	// // remove all childs from original node:
-	// while (node.firstChild) {
-	// 	node.removeChild(node.firstChild);
-	// }
-	//
-	// // append new node structure.
-	// newNode.childNodes.forEach(child => {
-	// 	node.appendChild(child);
-	// });
-	//
-	// newNode.empty();
+		// get the position of the next prefix and suffix.
+		let prefix = GetFirstMatch(text, PREFIX);
+		let suffix = GetFirstMatch(text, SUFFIX);
 
-	nodeStack.pop();
+		if (prefix == null && suffix == null) {
+			// no matches;
+			continue;
+		}
+
+		let nextPrefixPosition = prefix != null ? prefix.index : Number.POSITIVE_INFINITY;
+		let nextSuffixPosition = suffix != null ? suffix.index : Number.POSITIVE_INFINITY;
+
+		if (nextPrefixPosition == nextSuffixPosition) {
+			// should never be the case but idk.
+			console.error("fatal: nextPrefixPosition and nextSuffixPosition are the same but not infinity!!: " + `${nextPrefixPosition}`);
+
+			return node;
+		}
+
+		if (nextPrefixPosition < nextSuffixPosition) {
+			// next is prefix
+			prefix = prefix!;
+
+			let textBeforeDelim = text.slice(0, nextPrefixPosition);
+			let textAfterDelim = text.slice(prefix.end);
+			let prefixContent = prefix.value;
+			let color = prefixContent.slice(3, prefixContent.length - 1);
+
+			console.log(`handling prefix:\nprevText: ${textBeforeDelim}\nnextText: ${textAfterDelim}`)
+
+			// create the color element
+			let colorSpan = document.createElement("span");
+			colorSpan.addClass(`${CSS_COLOR_PREFIX}${themeName}-${color}`);
+
+			// set text in last node and create color node and insert it after the last node. 
+			childNode.nodeValue = textBeforeDelim;
+			childNode.parentNode?.insertAfter(colorSpan, childNode);
+
+			// add the rest of the text afterwards.
+			let newNode = document.createTextNode(textAfterDelim);
+			colorSpan.appendChild(newNode);
+
+			// set the colorSpan to be the new parent.
+			nodeStack.push(colorSpan);
+
+			continue;
+		}
+		// next is suffix;
+
+		let textBeforeDelim = text.slice(0, nextSuffixPosition);
+		let textAfterDelim = text.slice(suffix!.end);
+
+		console.log(`handling suffix:\nprevText: ${textBeforeDelim}\nnextText: ${textAfterDelim}`);
+
+		childNode.nodeValue = textBeforeDelim;
+
+		let prevNode = nodeStack.pop()!;
+
+		let newNode = document.createTextNode(textAfterDelim);
+		prevNode.parentNode?.insertAfter(newNode, prevNode);
+
+		continue;
+
+	}
 	return node;
-
 }
 
 function prettyPrintDOMStructure(node: Node, depth: number = 0): string {
@@ -324,6 +211,13 @@ function prettyPrintDOMStructure(node: Node, depth: number = 0): string {
 	return output;
 }
 
+/**
+ * get a list of all matches for a regex in a given string
+ *
+ * @param {string} text - the text to be matched
+ * @param {RegExp} regex - regex used for matching. State of regex is not changed.
+ * @returns {RegExMatch[]} The list of all matches in the given string.
+ */
 function GetAllMatches(text: string, regex: RegExp): RegExMatch[] {
 	// so in the end i guess it does have to be global.
 	// Regex being statefull is just so annoying...
@@ -343,6 +237,30 @@ function GetAllMatches(text: string, regex: RegExp): RegExMatch[] {
 	}
 
 	return matches;
+}
+
+/**
+ * get the first match for a regex and a given text.
+ *
+ * @param {string} text - the text to be matched
+ * @param {RegExp} regex - the regex to be used. state of regex is not changed.
+ * @returns {RegExMatch | null} the first match or null
+ */
+function GetFirstMatch(text: string, regex: RegExp): RegExMatch | null {
+	const regexCopy = new RegExp(regex.source, 'g');
+	const matches: RegExMatch[] = [];
+
+	let m = regexCopy.exec(text);
+
+	if (m !== null) {
+		return {
+			index: m.index,
+			value: m[0],
+			end: m.index + m[0].length
+		};
+	}
+
+	return null;
 }
 
 
